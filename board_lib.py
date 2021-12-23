4a1985aed7a520d1fd81155cdb3e562c566a649c
bjorrain <bjorrain@bk.ru>
2021-12-23 17:35:56 +0300

    reformatted via yapf

--- board_lib.py
+++ board_lib.py
@@ -1,13 +1,12 @@
-
-#MODULE INFO BLOCK 
+#MODULE INFO BLOCK
 MODULE_NAME = 'libboard'
 MODULE_VERSION = '0.0.1_vhm'
-
 
 import pretty_out
 import pygame
 
 logger = pretty_out.Log()
+
 
 class Board:
     # создание поля
@@ -21,8 +20,12 @@ class Board:
         logger.start_message(MODULE_NAME, MODULE_VERSION)
 
     def make_board(self):
-        self.board = [[Cell(self.left + (i * self.cell_size), self.top + (j * self.cell_size), self.cell_size) for j in
-          range(self.height)] for i in range(self.width)]
+        self.board = [[
+            Cell(self.left + (i * self.cell_size),
+                 self.top + (j * self.cell_size), self.cell_size)
+            for j in range(self.height)
+        ] for i in range(self.width)]
+
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -31,18 +34,25 @@ class Board:
         self.make_board()
 
     def render(self, screen: pygame.Surface):
-        pygame.draw.rect(screen, color=pygame.Color('white'),
-                         rect=(self.left, self.top,self.cell_size * self.width,
-                  self.cell_size * self.height), width=1)
+        pygame.draw.rect(screen,
+                         color=pygame.Color('white'),
+                         rect=(self.left, self.top,
+                               self.cell_size * self.width,
+                               self.cell_size * self.height),
+                         width=1)
 
         for i in range(self.width):
             for j in range(self.height):
-               self.board[i][j].render(screen)
-    def get_cell_by_position(self, x_pos: int, y_pos:  int):
+                self.board[i][j].render(screen)
+
+    def get_cell_by_position(self, x_pos: int, y_pos: int):
         try:
-            return self.board[x_pos - self.left // self.cell_size][y_pos - self.top // self.cell_size]
+            return self.board[x_pos -
+                              self.left // self.cell_size][y_pos - self.top //
+                                                           self.cell_size]
         except:
             return None
+
     def on_click(self, x_pos: int, y_pos: int):
         self.get_cell_by_position(x_pos, y_pos).on_click()
 
@@ -53,7 +63,7 @@ class Cell:
         self.rect = ((x_pos, y_pos), (size, size))
 
     def add_element(self, element, element_name: str):
-        try: 
+        try:
             self.elements.append({element_name: element})
         except Exception as E:
             logger.error(MODULE_NAME, E)
@@ -70,8 +80,11 @@ class Cell:
         return list(self.elements.keys())
 
     def render(self, screen: pygame.Surface):
-        pygame.draw.rect(screen, color=pygame.Color('white'),
-                         rect=self.rect, width=1)
+        pygame.draw.rect(screen,
+                         color=pygame.Color('white'),
+                         rect=self.rect,
+                         width=1)
         self.elements[0].render()
+
     def on_click(self, *args, **kwargs):
         self.elements[0].on_click(args, kwargs)
--- pretty_out.py
+++ pretty_out.py
@@ -4,10 +4,12 @@ class Log:
             self.out = print
         else:
             self.out = open(path, 'ra').write
+
     def error(self, module_name: str, text: str):
         self.out(f'[ERROR from {module_name.upper()}]: {text}')
+
     def diag(self, module_name, text):
         self.out(f'[DIAG from {module_name.upper()}]: {text}')
-    def start_message(self, module_name:str, module_version: str):
-         self.out(f'[STARTED {module_name.upper()} ver {module_version}')
-    
+
+    def start_message(self, module_name: str, module_version: str):
+        self.out(f'[STARTED {module_name.upper()} ver {module_version}')
